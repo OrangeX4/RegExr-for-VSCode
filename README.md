15ed4ccdcd35a55e70a02952c09fa5486e578035
OrangeX4 <318483724@qq.com>
2021-04-01 23:09:40 +0800

    Complete

--- README.md
+++ README.md
@@ -1,65 +1,22 @@
-# regexr-vscode README
+# RegExr for VSCode
 
-This is the README for your extension "regexr-vscode". After writing up a brief description, we recommend including the following sections.
+## About
 
-## Features
+[RegExr.com](https://regexr.com) is an online tool to learn, build, and test Regular Expressions. It was created by [Grant Skinner](https://twitter.com/gskinner) and the nice people at [gskinner.com](http://gskinner.com/).
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+You can browse RegExr in vscode natively via this extension.
 
-For example if there is an image subfolder under your extension project workspace:
+The best regular expressions tool!
 
-\!\[feature X\]\(images/feature-x.png\)
+## Usage
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+1. Install the extension.
+2. Press `Shift + Ctrl + P`.
+3. Input `regexr`.
+4. Press `Enter` and run it.
 
-## Requirements
+![](https://pic3.58cdn.com.cn/nowater/webim/big/n_v26d930d29eb3148bfa63b84cbec3726f6.png)
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+## Screenshot
 
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+![](https://pic3.58cdn.com.cn/nowater/webim/big/n_v2e07432111d22422389627bf1ddc523e8.png)
--- extension.js
+++ extension.js
@@ -1,37 +1,58 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const path = require('path');
+const fs = require('fs');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+
+// function getExtensionFileVscodeResource(context, relativePath) {
+// 	const diskPath = vscode.Uri.file(path.join(context.extensionPath, relativePath));
+// 	return diskPath.with({ scheme: 'vscode-resource' }).toString();
+// }
+
+/**
+ * 从某个HTML文件读取能被Webview加载的HTML内容
+ * @param {*} context 上下文
+ * @param {*} templatePath 相对于插件根目录的html文件相对路径
+ */
+ function getWebViewContent(context, templatePath) {
+	const resourcePath = path.join(context.extensionPath, templatePath);
+	const dirPath = path.dirname(resourcePath);
+	let html = fs.readFileSync(resourcePath, 'utf-8');
+	// vscode不支持直接加载本地资源，需要替换成其专有路径格式，这里只是简单的将样式和JS的路径替换
+	html = html.replace(/(<link.+?href="|<script.+?src="|<img.+?src=")(.+?)"/g, (m, $1, $2) => {
+		return $1 + vscode.Uri.file(path.resolve(dirPath, $2)).with({ scheme: 'vscode-resource' }).toString() + '"';
+	});
+	return html;
+}
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "regexr-vscode" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('regexr-vscode.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from regexr-vscode!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.commands.registerCommand('regexr-vscode.regexr', function () {
+        // 创建webview
+        const panel = vscode.window.createWebviewPanel(
+            'testWebview', // viewType
+            "RegExr", // 视图标题
+            vscode.ViewColumn.One, // 显示在编辑器的哪个部位
+            {
+                enableScripts: true, // 启用JS，默认禁用
+                retainContextWhenHidden: true, // webview被隐藏时保持状态，避免被重置
+            }
+        );
+        panel.webview.html = getWebViewContent(context, 'deploy/index.html');
+    }));
 }
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
-	activate,
-	deactivate
+    activate,
+    deactivate
 }
